5c58a9c3d223b22a4b34a207df6dc3fc98da7f28
Nico Steven101 <castroconi101@gmail.com>
2025-10-17 11:46:15 +0800

    HeaderDiv Polished

--- client/src/components/startComponents/HeaderDiv.jsx
+++ client/src/components/startComponents/HeaderDiv.jsx
@@ -5,35 +5,34 @@ const moles = "absolute bottom-10 w-20 h-10 bg-white rounded-full"
 
 export default function BlockHeader() {
   return (
-    <div className="relative flex flex-col items-center justify-center text-white bg-gradient-to-r from-orange-500 to-red-500 w-full h-[60vh] overflow-hidden">
-      
-      <div className="absolute top-3 left-4">
-        <NavLink 
-          to="/" 
-          className="text-3xl font-bold transition-transform duration-300 hover:scale-110"
-        >
-          Craftbook
-        </NavLink>
-      </div>
+<div className="relative flex flex-col items-center justify-center text-white bg-[linear-gradient(90deg,#ff880c,#ef290e)] w-full h-[60vh] overflow-hidden">
+<div className="absolute top-3 left-4">
+  <NavLink 
+    to="/" 
+    className="text-3xl font-bold transition-transform duration-300 hover:scale-110"
+  >
+    Craftbook
+  </NavLink>
+</div>
 
-      <div className="absolute top-3 right-5">
-        <NavLink 
-          to="/about" 
-          className="text-3xl font-bold transition-transform duration-300 hover:scale-110"
-        >
-          About
-        </NavLink>
-      </div>
+<div className="absolute top-3 right-5">
+  <NavLink 
+    to="/about" 
+    className="text-3xl font-bold transition-transform duration-300 hover:scale-110"
+  >
+    About
+  </NavLink>
+</div>
 
-      <hr className="absolute top-8 w-4/5 left-1/2 -translate-x-1/2 border-t-4 border-orange-300 opacity-90"/>
+<hr className="absolute top-[2rem] left-[11rem] right-[8rem] border-t-4 border-orange-300 opacity-90" />
 
       {/* Main title */}
-<h1 className="relative text-[30rem] font-bold tracking-tight leading-none mt-10 font-spartan -translate-x-10">
+<h1 className="relative right-[8vw] text-[20vw] mt-[1.5rem] font-bold tracking-tight leading-none font-spartan">
   CRAFTERIA
 </h1>
 
       {/* Bottom text */}
- <p className="text-3xl font-semibold italic mt-4">
+ <p className="relative text-[2vw] bottom-11 font-semibold ">
         What would you like to craft today?
       </p>
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import BlockHeader from '../components/startComponents/HeaderDiv'
 
-
 export default function Home() {
-    return ( <> 
-    <BlockHeader></BlockHeader>
-    <div>
-    <p>Hi! This is Home</p>
+  return (
+    <div className="min-h-screen w-full bg-[radial-gradient(circle_at_50%_60%,#fa5f07,#ffc187,#faf0c4,#ffdc42)] flex flex-col items-center">
+      <BlockHeader />
+
+      <div className="mt-10 text-center">
+        <p className="text-2xl font-semibold text-gray-800">REGISTER OR LOG IN</p>
+      </div>
     </div>
-    </>
-    )
+  )
 }
